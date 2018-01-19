8c8ab7e2109a2b900cfa7844513db5c29882815d
Sifu Biri <sifeddinebiri@gmail.com>
2018-01-18 22:44:16 -0600

    hooked up firebase with state transition

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, } from 'react-native';
 
 import firebase from 'firebase';
 
 
-import { Header } from './src/components/common';
+import { Header, Button, Spinner, CardSection } from './src/components/common';
 import LoginForm from './src/components/LoginForm';
 
 class App extends Component {
+  state = { logedIn: null };
+
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyDOcxGdDKqd6szyqT1mNZric1FCUzOUkOE',
@@ -17,13 +20,39 @@ class App extends Component {
       storageBucket: 'auth-29097.appspot.com',
       messagingSenderId: '647397469378'
     });
+
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ logedIn: false });
+      } else {
+        this.setState({ logedIn: true });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.logedIn) {
+      case false:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log out
+        </Button>
+        );
+      case true:
+        return <LoginForm />;
+
+      default:
+        return <Spinner size='large' />;
+
+    }
   }
 
   render() {
     return (
-      <View>
+      <View style={{ flowDirection: 'row' }}>
         <Header headerText='AUTH' />
-        <LoginForm /> 
+        {this.renderContent()}
       </View>
     );
   }
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,23 +2,51 @@ import React, { Component } from 'react';
 import { StyleSheet, Text } from 'react-native';
 import firebase from 'firebase';
 
-import { Card, CardSection, Button, Input } from './common/';
+import { Card, CardSection, Button, Input, Spinner } from './common/';
 
 class LoginForm extends Component {
-    state = { email: '', password: '', error: '' };
+    state = { email: '', password: '', error: '', loading: false };
 
     onButtonPress() {
         const { email, password } = this.state;
 
-        this.setState({ error: '' });
+        this.setState({ error: '', loading: true });
 
         firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(this.onLoginSucess.bind(this))
             .catch(() => {
                 firebase.auth().createUserWithEmailAndPassword(email, password)
-                    .catch(() => {
-                        this.setState({ error: 'Authentication Failed' });
-                    });
+                    .then(this.onLoginSucess.bind(this))
+                    .catch(this.onLoginFail.bind(this));
             });
+    }
+
+    onLoginFail() {
+        this.setState({
+            error: 'Authentication Failed',
+            loading: false
+        });
+    }
+    
+    onLoginSucess() {
+        this.setState({
+            email: '',
+            password: '',
+            loading: false,
+            error: ''
+        });
+    }
+
+    renderButton() {
+        if (this.state.loading) {
+            return <Spinner size={'small'} />;
+        }
+
+        return (
+            <Button onPress={this.onButtonPress.bind(this)}>
+                Log in
+            </Button>
+        );
     }
 
     render() {
@@ -28,7 +56,7 @@ class LoginForm extends Component {
                     <Input
                         placeholder='user@gmail.com'
                         label='Email'
-                        text={this.state.email}
+                        value={this.state.email}
                         onChangeText={email => this.setState({ email })}
                     />
                 </CardSection>
@@ -37,7 +65,7 @@ class LoginForm extends Component {
                         secureTextEntry
                         placeholder='password'
                         label='Password'
-                        text={this.state.password}
+                        value={this.state.password}
                         onChangeText={password => this.setState({ password })}
                     />
                 </CardSection>
@@ -45,9 +73,7 @@ class LoginForm extends Component {
                     {this.state.error}
                 </Text>
                 <CardSection>
-                    <Button onPress={this.onButtonPress.bind(this)}>
-                        Log in
-                    </Button>
+                    {this.renderButton()}
                 </CardSection>
             </Card >
         );
